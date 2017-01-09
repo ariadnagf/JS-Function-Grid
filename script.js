8e5ffd4ac04a60365bfa508ec2e1122cb8162baf
ariadna <gomezariadna.ag@gmail.com>
2017-01-09 22:22:45 +0000

    fun on a grid

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Write your function calls below this line
+setColor(2, 0, "purple"); 
+setColor(0, 9, "green");
+setColor(0, 0, "green");
+setColor(9, 0, "green");
+setColor(9, 9, "green");
+
+setColor(1, 3, "yellow");
+setColor(1, 6, "yellow");
+setColor(4, 2, "yellow");
+setColor(5, 2, "yellow");
+setColor(6, 3, "yellow");
+setColor(6, 4, "yellow");
